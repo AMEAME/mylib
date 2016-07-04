24aff7b04fbb39a5710d762fd7f23697149cb82d
Ryuichiroh <an94_49@yahoo.co.jp>
2016-07-05 02:44:24 +0900

    a

---
+++ src/file.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void _file_error(char *file_name)
+{
+    FILE *fp;
+    if ((fp = fopen(file_name, "r")) == NULL)
+    {
+        printf("Cannot open input file '%s'", file_name);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int _file_get_size(char *file_name)
+{
+    _file_error(file_name);
+    FILE *fp = fopen(file_name, "r");
+    int counter = 0;
+    while (getc(fp) != EOF) counter++;
+    fclose(fp);
+    return counter;
+}
+
+#define read(file_name)                           \
+({                                                \
+    char *_readtext(char *file_name)              \
+    {                                             \
+        char *text = malloc(sizeof(char)          \
+            * (_file_get_size(file_name) + 1));   \
+        return _file_read_text(text, file_name);  \
+    }                                             \
+    _readtext(file_name);                         \
+})
+
+char *_file_read_text(char *text, char *file_name)
+{
+    _file_error(file_name);
+    FILE *fp = fopen(file_name, "r");
+    char ch;
+    while ((ch = getc(fp)) != EOF)
+    {
+        char s[] = {ch, '\0'};
+        strcat(text, s);
+    }
+    fclose(fp);
+    return text;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) return 1;
+    char *file_name = argv[1];
+    puts(read(file_name));
+    return 0;
+}
